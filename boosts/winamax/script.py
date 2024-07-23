90d370fcc429d75db7ad3c156b65e59d44cf98a0
Luis Teixeira Lima <dev.louwis@gmail.com>
2024-07-23 21:31:43 +0200

    fix bug

--- boosts/winamax/script.py
+++ boosts/winamax/script.py
@@ -49,25 +49,28 @@ async def winamax(bot):
                 betId = boost['betId']
                 outcomeId = boost['outcomes'][0]
 
-                match = [match for match in matches.values() if match['mainBetId'] == betId][0]
+                match = [match for match in matches.values() if match['mainBetId'] == betId][0] if matches else None
 
-                boost['intitule'] = outcomes[f'{outcomeId}']['label']
-                boost['odd'] = odds[f'{outcomeId}']
-                boost['title'] = match['title'].split(':', 1)[1].strip()
-                boost['startTime'] = datetime.fromtimestamp(match['matchStart'])
-                
-                finalBoosts.append({
-                    'betId': boost['betId'],
-                    'intitule': boost['intitule'],
-                    'boostedOdd': boost['odd'] if 'odd' in boost else 'N/A',
-                    'odd': boost['previousOdd'] if 'previousOdd' in boost else 'N/A',
-                    'title': boost['title'],
-                    'bigBoost': boost['marketId'] != 9038,
-                    'maxBet': boost['betTypeName'].lower().split('mise max ')[1].split(' €')[0],
-                    'sport': 'football',
-                    'betAnalytixBetName': f"{boost['title']} / {boost['intitule']}",
-                    'startTime': boost['startTime'].astimezone(pytz.utc).isoformat()
-                })
+                if match:
+                    boost['intitule'] = outcomes[f'{outcomeId}']['label']
+                    boost['odd'] = odds[f'{outcomeId}']
+                    boost['title'] = match['title'].split(':', 1)[1].strip()
+                    boost['startTime'] = datetime.fromtimestamp(match['matchStart'])
+                    
+                    finalBoosts.append({
+                        'betId': boost['betId'],
+                        'intitule': boost['intitule'],
+                        'boostedOdd': boost['odd'] if 'odd' in boost else 'N/A',
+                        'odd': boost['previousOdd'] if 'previousOdd' in boost else 'N/A',
+                        'title': boost['title'],
+                        'bigBoost': boost['marketId'] != 9038,
+                        'maxBet': boost['betTypeName'].lower().split('mise max ')[1].split(' €')[0],
+                        'sport': 'football',
+                        'betAnalytixBetName': f"{boost['title']} / {boost['intitule']}",
+                        'startTime': boost['startTime'].astimezone(pytz.utc).isoformat()
+                    })
+                else:
+                    logging.warning(f"Match not found for betId {betId}")
             
             await publish_boosts('winamax', bot, finalBoosts, '0xff0000')
 
