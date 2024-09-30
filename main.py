f890660c2c9c41b2bb6fc5fb36ceb400d1b3854b
Luis Teixeira Lima <dev.louwis@gmail.com>
2024-09-30 18:00:46 +0200

    add commands

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print(discord.__version__)
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 tree = app_commands.CommandTree(bot)
-SERVER_ID = os.getenv('SERVER_ID')
+MT_SERVER_ID = os.getenv('MT_SERVER_ID')
 
 
 # @tree.command(
@@ -39,10 +39,63 @@ async def on_message(message):
 async def boosts():
     await search_boosts(bot)
 
+# create a slashcommands function that will return the diffenrece in % between two numbers in params
+@tree.command(
+    name="value",
+    description="Calculate la value et la mise à jouer de la trueodds et de la cote prise",
+    guild=discord.Object(id=MT_SERVER_ID)
+)
+async def value(interaction, trueodds: float, taken_odds: float):
+    value = (taken_odds - trueodds) / trueodds * 100 
+    kelly = value / (taken_odds - 1)
+
+
+    # make a message to show the percentage value and the 4 types of kelly
+    # make an embed it will be easier
+
+    embed = discord.Embed(
+        title=f"Calcul de value et de mise",
+        description=f"Trueodds: {trueodds}\nCote prise: {taken_odds}\nValue est de {value:.2f}%",
+        color=0xFFFFFF
+    )
+
+    embed.add_field(name="Kelly/2", value=f"{kelly/2:.2f}%", inline=True)
+    embed.add_field(name="Kelly/3", value=f"{kelly/3:.2f}%", inline=True)
+    embed.add_field(name="Kelly/4", value=f"{kelly/4:.2f}%", inline=True)
+
+    await interaction.response.send_message(embed=embed, ephemeral=True)
+
+@tree.command(
+    name="public_value",
+    description="Calculate la value et la mise à jouer de la trueodds et de la cote prise",
+    guild=discord.Object(id=MT_SERVER_ID)
+)
+async def public_value(interaction, trueodds: float, taken_odds: float):
+    value = (taken_odds - trueodds) / trueodds * 100 
+    kelly = value / (taken_odds - 1)
+
+
+    # make a message to show the percentage value and the 4 types of kelly
+    # make an embed it will be easier
+
+    embed = discord.Embed(
+        title=f"Calcul de value et de mise",
+        description=f"Trueodds: {trueodds}\nCote prise: {taken_odds}\nValue: {value:.2f}%",
+        color=0xFFFFFF
+    )
+
+    embed.add_field(name="Kelly/2", value=f"{kelly/2:.2f}%", inline=True)
+    embed.add_field(name="Kelly/3", value=f"{kelly/3:.2f}%", inline=True)
+    embed.add_field(name="Kelly/4", value=f"{kelly/4:.2f}%", inline=True)
+
+    await interaction.response.send_message(embed=embed, ephemeral=False)    
+
 @bot.event
 async def on_ready():
     # await tree.sync(guild=discord.Object(id=SERVER_ID))
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    boosts.start()
+    # boosts.start()
+
+    await tree.sync(guild=discord.Object(id=MT_SERVER_ID))
 
 bot.run(os.getenv('DISCORD_TOKEN'))
